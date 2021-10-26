046444eaffa412eeb9ea97e55c2920fa94a2060c
KotaGit <Kota7arai@gmail.com>
2021-10-26 23:29:44 +0900

    プロジェクト ファイルを追加します。

---
+++ Sample2/Sample2.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+class Sample2
+{
+    public static void Main()
+    {
+        Form fm;
+        fm = new Form();
+
+        fm.Text = "ようこそ";
+
+        Application.Run(fm);
+    }
+}
